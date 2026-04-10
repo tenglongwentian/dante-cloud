e362285cb468c941671a72433767e838c3e4e8b4
码匠君 <pointer_v@qq.com>
2026-04-10 14:52:40 +0800

    [重构] 重构系统核心数据存储以及 SAS 数据存储相关代码，提取独立模块与企业版对齐，提升扩展为其它存储形式的便捷性

--- dante-cloud-services/dante-cloud-oss-ability/src/main/resources/sqls/oss-data-postgresql.sql
+++ dante-cloud-services/dante-cloud-oss-ability/src/main/resources/sqls/oss-data-postgresql.sql
@@ -319,30 +319,17 @@ INSERT INTO "sys_element_role" ("element_id", "role_id") VALUES ('9f064902-162a-
 INSERT INTO "sys_element_role" ("element_id", "role_id") VALUES ('9f064902-162a-408f-bd3e-866b03c9dae1', '1');
 INSERT INTO "sys_element_role" ("element_id", "role_id") VALUES ('9f064902-162a-408f-bd3e-866b03c9dae1', 'faccc981-7cd4-4333-9ef9-93752eb186bd');
 
-/*
- * Copyright 2020-2030 码匠君<herodotus@aliyun.com>
- *
- * Dante Cloud licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Dante Cloud 采用 APACHE LICENSE 2.0 开源协议，您在使用过程中，需要注意以下几点：
- *
- * 1. 请不要删除和修改根目录下的LICENSE文件。
- * 2. 请不要删除和修改 Dante Cloud 源码头部的版权声明。
- * 3. 请保留源码和相关描述文件的项目出处，作者声明等。
- * 4. 分发源码时候，请注明软件出处 <https://gitee.com/dromara/dante-cloud>
- * 5. 在修改包名，模块名称，项目代码等时，请注明软件出处 <https://gitee.com/dromara/dante-cloud>
- * 6. 若您的项目无法满足以上几点，可申请商业授权
- */
+-- ----------------------------
+-- Table data for oauth2_authorization_resource
+-- ----------------------------
+
+INSERT INTO "oauth2_authorization_resource" ("id", "client_id", "client_name", "redirect_uris", "logo")
+SELECT C.id                                                                    AS id,
+       C.client_id,
+       C.client_name,
+       C.redirect_uris,
+       (SELECT A.logo FROM oauth2_application A WHERE A.application_id = C.id) AS logo
+FROM oauth2_registered_client C;
 
 -- ----------------------------
 -- Table data for sys_interface_authority
